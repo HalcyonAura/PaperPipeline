5302b9807c6337c6e1f1795f226f349ac0aae1bf
Cecilia La Place <laplace95@gmail.com>
2022-11-03 12:49:30 -0700

    Formatting and consistency review, cleaned comments and README

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { resolve } from "path"
-import { config } from "dotenv"
-config({ path: resolve(__dirname, "../.env") })
+import { resolve } from "path";
+import { config } from "dotenv";
+config({ path: resolve(__dirname, "../.env") });
 
-import Airtable, { FieldSet } from 'airtable';
-import axios, { AxiosRequestConfig, AxiosPromise, AxiosResponse } from 'axios';
+import Airtable from 'airtable';
+import axios, { AxiosResponse } from 'axios';
 import { AirtableBase } from 'airtable/lib/airtable_base';
 Airtable.configure({
   endpointUrl: 'https://api.airtable.com',
@@ -11,10 +11,10 @@ Airtable.configure({
 });
 const base: AirtableBase = Airtable.base(process.env.AIRTABLE_BASE_ID as string);
 
-const zoteroKey = process.env.ZOTERO_API_KEY as string
-const zoteroUser = "https://api.zotero.org/users/"+ process.env.ZOTERO_USER_ID
+const zoteroKey = process.env.ZOTERO_API_KEY as string;
+const zoteroUser = "https://api.zotero.org/users/"+ process.env.ZOTERO_USER_ID;
 
-/** 
+/**
  * Interfaces
  */
 interface Creator {
@@ -36,6 +36,23 @@ interface ZoteroRecord {
   }
 }
 
+interface RefFields {
+  "fields": {
+    "Key": string,
+    "Title": string,
+    "Authors": string,
+    "Type": string,
+  }
+}
+interface QuoteFields {
+  "fields": {
+    "Key": string,
+    "Quote": string,
+    "Color": string,
+    "Page": number
+  }
+}
+
 interface Quote {
   quote: string,
   page: number
@@ -49,32 +66,28 @@ interface Annotations {
 
 /**
  * Processing
- */ 
+ */
 /**
- * 
- * @param {array of authors of entry} creators 
- * @returns string of authors separated by ;
+ * @param {array} creators - authors
+ * @returns {string}
  */
  const parseCreators = (creators: Creator[]): string => {
-  let creatorsString: string = ""
+  let creatorsString: string = "";
   if (creators.length != 0)
-    creatorsString = creators[0].lastName + ", " + creators[0].firstName
-  
+    creatorsString = creators[0].lastName + ", " + creators[0].firstName;
   for (let j = 1; j < creators.length; j++){
-    creatorsString+= "; " +creators[j].lastName + ", " + creators[j].firstName
+    creatorsString+= "; " +creators[j].lastName + ", " + creators[j].firstName;
   }
-  return creatorsString
+  return creatorsString;
 } // TODO: adjust this function for eventual authors table and organizing by unique authors
 
 /**
- * 
- * @param note 
- * @returns map of annotations
+ * @param {string} note - HTML string
+ * @returns {Annotations}
  */
 const parseAnnotations = (note: string): Annotations => {
   //ignore first set of HTML tags up to first <p>
-  let tags: string[] = note.split("<p>").slice(1)
-  // clean up content
+  let tags: string[] = note.split("<p>").slice(1);
   let map: Annotations = {
       Blue: [],
       Green: [],
@@ -82,60 +95,67 @@ const parseAnnotations = (note: string): Annotations => {
     };
   for (let i = 0; i < tags.length; i++){
     // Get highlight color
-    let idx = tags[i].indexOf("background-color: ") + ("background-color: ".length) + 1 // for #
-    let color = tags[i].substring(idx, idx+8)
+    let idx = tags[i].indexOf("background-color: ") + ("background-color: ".length) + 1; // for #
+    let color = tags[i].substring(idx, idx+8);
     idx += 9 //jump forward to end of span color
-    let quoteIdx = tags[i].indexOf("“", idx)
-    let QuoteEndIdx = tags[i].indexOf("”", idx)
-    let quoteText:string = tags[i].substring(quoteIdx+1, QuoteEndIdx) // ignore alternate quotation marks
-    idx = QuoteEndIdx
-    let pageIdx = tags[i].indexOf("p. ", idx)
-    let pageEndIdx = tags[i].indexOf("</span", pageIdx)
-    let page = +tags[i].substring(pageIdx + "p. ".length, pageEndIdx)
-    console.log(quoteText)
-    console.log(color)
-    console.log(page)
+    let quoteIdx = tags[i].indexOf("“", idx);
+    let QuoteEndIdx = tags[i].indexOf("”", idx);
+    let quoteText:string = tags[i].substring(quoteIdx+1, QuoteEndIdx); // ignore special quotation marks
+    idx = QuoteEndIdx;
+    let pageIdx = tags[i].indexOf("p. ", idx);
+    let pageEndIdx = tags[i].indexOf("</span", pageIdx);
+    let page = +tags[i].substring(pageIdx + "p. ".length, pageEndIdx);
+    console.log(quoteText);
+    console.log(color);
+    console.log(page);
     if (color == "83ffed80") {
-    map["Blue"].push({quote: quoteText, page: page})
+    map["Blue"].push({quote: quoteText, page: page});
     } else if (color == "00ff0080") {
-    map["Green"].push({quote: quoteText, page: page})
+    map["Green"].push({quote: quoteText, page: page});
     } else if (color == "ffff0080") {
-    map["Yellow"].push({quote: quoteText, page: page})
-    } 
+    map["Yellow"].push({quote: quoteText, page: page});
+    }
   }
   return map
 }
 
-const createRefField = (data: ZoteroRecord) => {
+/**
+ * @param {ZoteroRecord} data
+ * @returns {RefFields}
+ */
+const createRefField = (data: ZoteroRecord): RefFields => {
   return  {"fields": {
     "Key": data.key,
     "Title": data.data.title,
     "Authors": parseCreators(data.data.creators),
     "Type": data.data.itemType,
   }}
-} // TODO return interface
+} // TODO Add children?
 
-const createQuoteField = (key: string, parentKey: string, color: string, quote: string, page: number) => {
+/**
+ * @param {ZoteroRecord} data - Zotero record data
+ * @returns {RefFields}
+ */
+const createQuoteField = (key: string, parentKey: string, color: string, quote: string, page: number): QuoteFields => {
   return  { "fields": {
     "Key": key,
     "Quote": quote,
     "Color": color,
     "Page": page
   }}
-} // TODO return interface, add parents
+} // TODO add parents
 
 /*** Zotero
  * Get recently uploaded or modified entries or children of entries
  */
 
 /**
- * 
- * @param {number of entries to query} amt 
+ * @param {number} amt - entries to query for
  */
- const getMostRecentPapersUpdated = 
+ const getMostRecentPapersUpdated =
   (amt: number): Promise<AxiosResponse<any, any>> => {
   try {
-    let url = zoteroUser + "/items/top"
+    let url = zoteroUser + "/items/top";
     return axios.get(url, {
       headers: {
           "Zotero-API-Key": zoteroKey,
@@ -148,18 +168,17 @@ const createQuoteField = (key: string, parentKey: string, color: string, quote: 
       }
     })
   } catch (error) {
-    throw error
+    throw error;
   }
  }
 
-  /**
- * 
- * @param {number of entries to query} amt 
+/**
+ * @param {number} amt - entries to query for
  */
-const getMostRecentPapersAdded = 
+const getMostRecentPapersAdded =
 (amt: number): Promise<AxiosResponse<any, any>> => {
   try {
-    let url = zoteroUser + "/items/top"
+    let url = zoteroUser + "/items/top";
     return axios.get(url, {
       headers: {
           "Zotero-API-Key": zoteroKey,
@@ -172,18 +191,17 @@ const getMostRecentPapersAdded =
       }
     })
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
 /**
- * 
- * @param key Zotero record
+ * @param {string} key - Zotero record
  * @returns record's children
  */
 const getItem = (key: string): Promise<AxiosResponse<any, any>> => {
   try {
-    let url = zoteroUser + "/items/" + key + "/children"
+    let url = zoteroUser + "/items/" + key + "/children";
     return axios.get(url, {
         headers: {
             "Zotero-API-Key": zoteroKey,
@@ -193,7 +211,7 @@ const getItem = (key: string): Promise<AxiosResponse<any, any>> => {
         }
     })
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
@@ -202,15 +220,14 @@ const getItem = (key: string): Promise<AxiosResponse<any, any>> => {
  */
 
 /**
-* 
-* @param data 
-* @returns 
-*/
+ * @param {string} key - Zotero record
+ * @returns
+ */
 const putReferences = async (data: ZoteroRecord[]) => {
   try {
     let fields: any = [] // TODO figure this type out
     for (let i = 0; i < data.length; i++){
-      fields.push(createRefField(data[i]))
+      fields.push(createRefField(data[i]));
     }
     // Max 10 objects per create records
     const promises = [];
@@ -219,28 +236,27 @@ const putReferences = async (data: ZoteroRecord[]) => {
     }
     await Promise.all(promises);
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
 /**
-* 
-* @param data 
-* @returns 
-*/
+ * @param {string} key - Zotero record
+ * @returns
+ */
  const putQuotes = async (data: ZoteroRecord) => {
   try {
-    let fields: any = [] // TODO figure this type out
+    let fields: any = []; // TODO figure this type out
     // Don't process non-notes for quotes
     if (data.data.itemType != 'note') {
-      return false
+      return;
     }
     let mapQuotes: Annotations = parseAnnotations(data.data.note);
     Object.entries(mapQuotes).forEach(entry => {
       const [color, quotes] = entry;
       for (let j = 0; j < quotes.length; j++){
         fields.push(
-          createQuoteField(data.key, data.data.parentItem, color, quotes[j].quote, quotes[j].page))
+          createQuoteField(data.key, data.data.parentItem, color, quotes[j].quote, quotes[j].page));
       }
     });
     // Max 10 objects per create records
@@ -249,22 +265,20 @@ const putReferences = async (data: ZoteroRecord[]) => {
       promises.push(base('Quotes').create(fields.slice(i*10, (i*10)+10)));
     }
     await Promise.all(promises);
-    return true
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
 /**
-* 
-* @param data 
-* @returns 
-*/
+ * @param {string} key - Zotero record
+ * @returns
+ */
 const updateReferences = async (data: ZoteroRecord[]) => {
   try {
-    let fields: any = [] // TODO figure this type out
+    let fields: any = []; // TODO figure this type out
     for (let i = 0; i < data.length; i++){
-      fields.push(createRefField(data[i]))
+      fields.push(createRefField(data[i]));
     }
     // Max 10 objects per create records
     const promises = [];
@@ -273,41 +287,38 @@ const updateReferences = async (data: ZoteroRecord[]) => {
     }
     await Promise.all(promises);
   } catch (error) {
-    console.error(error)
+    throw error;
   }
 }
 
 /**
-* 
-* @param data 
-* @returns 
-*/
+ * @param {string} key - Zotero record
+ * @returns
+ */
 const updateQuotes = async (data: ZoteroRecord) => {
   try {
-    let fields: any = [] // TODO figure this type out
+    let fields: any = []; // TODO figure this type out
+    // TODO Check if quote exists in table yet
     // Don't process non-notes for quotes
     if (data.data.itemType != 'note') {
-      return false
+      return;
     }
     let mapQuotes: Annotations = parseAnnotations(data.data.note);
     Object.entries(mapQuotes).forEach(entry => {
       const [color, quotes] = entry;
       for (let j = 0; j < quotes.length; j++){
         fields.push(
-          createQuoteField(data.key, data.data.parentItem, color, quotes[j].quote, quotes[j].page))
+          createQuoteField(data.key, data.data.parentItem, color, quotes[j].quote, quotes[j].page));
       }
     });
-
     // Max 10 objects per create records
     const promises = [];
     for (let i = 0; i < fields.length; i += 10){
       promises.push(base('Quotes').create(fields.slice(i*10, (i*10)+10)));
     }
     await Promise.all(promises);
-
-    return true
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
@@ -316,71 +327,59 @@ const updateQuotes = async (data: ZoteroRecord) => {
  */
 
 /**
- * 
- * @param amt 
+ * @param {number} amt - entries to query for
+ * @returns
  */
 const recentlyAdded = async (amt: number) => {
   try {
     let response = await getMostRecentPapersAdded(amt);
-    putReferences(response.data)
+    putReferences(response.data);
     for(let item of response.data) {
-      let children = await getItem(item.key)
+      let children = await getItem(item.key);
       if (children) {
         for (let c of children.data){
-          putQuotes(c)
+          putQuotes(c);
         }
       }
     }
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
+/**
+ * @param {number} amt - entries to query for
+ * @returns
+ */
 const recentlyUpdated = async (amt: number) => {
   try {
     let response = await getMostRecentPapersUpdated(amt);
-    // Check if updated paper exists in table yet it no then put
+    // TODO Check if updated paper exists in table yet it no then put
     // needs to be sorted into what needs to be created and what doesn't
-    updateReferences(response.data)
-    if (response.data[0].key){
-      updateReferences(response.data[0].data)
+    updateReferences(response.data);
+    /*if (response.data[0].key){
+      updateReferences(response.data[0].data);
     } else {
-      putReferences(response.data[0].data)
-    }
+      putReferences(response.data[0].data);
+    }*/
     for(let item of response.data) {
-      let children = await getItem(item.key)
+      let children = await getItem(item.key);
       if (children){
         for (let c of children.data){
           // check if exists in table yet, if no then put
           if (c.data.key){
-            updateQuotes(c)
+            updateQuotes(c);
           } else {
-            putQuotes(c)
+            putQuotes(c);
           }
         }
       }
     }
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
-recentlyAdded(2)
-recentlyUpdated(2)
-
-// Use cases
-
-/*
-1. get most recent top level papers (updated)
-2. get children
-3. call updatereferences and put quotes
-
-1. get most recent top level papers (added)
-2. get children
-3. call putreferences and put quotes if notes available
-
-1. get most recent items
-2. if item is note find parent to update parent and call put quotes
-2a. if item is note call update quotes
-3. if item is ref call put ref and get children to call put quotes
-*/
+// Sample uses, run with yarn ts-node src/index.ts
+recentlyAdded(2);
+//recentlyUpdated(2);
